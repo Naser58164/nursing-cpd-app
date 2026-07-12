4bfb76ac910dc1f6a9ff5d6e198edaf5af900c0c
Naser58164 <n.r.a.s.58164@gmail.com>
2026-07-12 13:13:16 +0400

    config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 const CONFIG = {
     // 🔧 IMPORTANT: Replace this with your Google Apps Script deployment URL
-    API_URL: 'https://script.google.com/macros/s/AKfycbxED6oFqhaS1TNUlEZVQQrhoarzksf2y6AD-IgX0c5ooq2WHIKoh-uWuNCr73eQWdIVpA/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbxUeaPbRFQUuJExTR0o3Z0LvxmQCMRlt346PA6XzXWTSa5fDdIcdx3hvzBUI7Ry5JUELg/exec',
     
     // Application Settings
     APP_NAME: 'Nursing CPD Portal',
